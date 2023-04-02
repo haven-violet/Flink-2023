0c3a5082d1af7c3ea6a4df111bbf373d05623626
ljc <ljc123@qq.com>
2023-04-02 21:31:17 +0800

    log4j RollingFileAppender和DailyRollingFileAppender

--- src/main/java/com/sgg/log/Log4jTest.java
+++ src/main/java/com/sgg/log/Log4jTest.java
@@ -22,13 +22,17 @@ public class Log4jTest {
         //todo Log4j 默认日志级别是 Debug
         //1. 创建日志记录器对象
         Logger logger = Logger.getLogger(Log4jTest.class);
-        //日志级别
-        logger.fatal("fatal log"); //todo 严重错误, 一般会造成系统崩溃和终止运行
-        logger.error("error log"); //todo 错误信息, 但不会影响系统运行
-        logger.warn("warn log");   //todo 警告信息, 可能会发生问题
-        logger.info("info log");   //todo 程序运行信息, 数据库的连接、网络、IO操作等
-        logger.debug("debug log"); //todo 调试信息，一般在开发阶段使用, 记录程序的变量，参数等
-        logger.trace("trage log"); //todo 追踪信息, 记录程序的所有流程信息
+        int i = 9991;
+//        for (int i = 0; i < 5; i++) {
+
+            //日志级别
+            logger.fatal("fatal log -- " + i); //todo 严重错误, 一般会造成系统崩溃和终止运行
+            logger.error("error log -- " + i); //todo 错误信息, 但不会影响系统运行
+            logger.warn("warn log-- " + i);   //todo 警告信息, 可能会发生问题
+            logger.info("info log-- " + i);   //todo 程序运行信息, 数据库的连接、网络、IO操作等
+            logger.debug("debug log-- " + i); //todo 调试信息，一般在开发阶段使用, 记录程序的变量，参数等
+            logger.trace("trage log-- " + i); //todo 追踪信息, 记录程序的所有流程信息
+//        }
 
     }
 
