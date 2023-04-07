68cf93d48eadf6da151c2cef7ac62a9eca80f70b
ljc <ljc123@qq.com>
2023-04-07 21:31:52 +0800

    Slf4j日志门面 使用log4j具体日志实现

--- src/main/java/com/sgg/log/Log4j2Test.java
+++ src/main/java/com/sgg/log/Log4j2Test.java
@@ -3,6 +3,7 @@ package com.sgg.log;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
 
 /**
  * @BelongsProject: Flink-2023
@@ -14,6 +15,15 @@ import org.junit.Test;
  */
 public class Log4j2Test {
     public static final Logger LOGGER = LogManager.getLogger(Log4j2Test.class);
+    public static final org.slf4j.Logger log = LoggerFactory.getLogger(Log4j2Test.class);
+
+    @Test
+    public void testLog4j2_slf4j() {
+        log.info("log4j---");
+        log.debug("debug --- ");
+        log.trace("trace =-- ");
+    }
+
     @Test
     public void testLog4j() {
         LOGGER.fatal("fatal");
